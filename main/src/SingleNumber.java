d4bcdfc7573c5f4418d79a72586cdf92bbbec2c7
Zhanyl <123janyl456@gmail.com>
2026-01-16 22:12:23 +0100

    added leetcode 136

---
+++ main/src/SingleNumber.java
@@ -0,0 +1,12 @@
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        int ans=0;
+        for(int x:nums){
+            ans=ans^x;
+        }
+        return ans;
+
+        }
+
+}
