c19324ec6b1915ab3c2bd88e0c488172352ed6be
Zhanyl <123janyl456@gmail.com>
2026-02-14 08:06:06 +0100

    improve 977

---
+++ main/src/SquaresOfSortedArray_977.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class SquaresOfSortedArray_977 {
+    public int[] sortedSquares(int[] nums) {
+        int n=nums.length;
+        int[] result= new int[n];
+        int left =0;
+        int right=n-1;
+
+        for(int i = n-1; i>=0; i--){
+            if(Math.abs(nums[right])<Math.abs(nums[left])){
+                result[i]=nums[left]*nums[left];
+                left++;
+
+            }
+            else{
+                result[i]=nums[right]*nums[right];
+                right--;
+            };
+
+        }
+        return result;
+
+
+
+    }
+}
