537a6c38aede006158921621c7e494ec429b4992
Zhanyl <123janyl456@gmail.com>
2026-02-18 21:03:54 +0100

    leetcode 189

---
+++ main/src/RotateArray_189.java
@@ -0,0 +1,25 @@
+import java.sql.Array;
+import java.util.List;
+
+public class RotateArray_189 {
+    public void rotate(int[] nums, int k) {
+        if(k> nums.length){
+            k=k%nums.length;
+        }
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
+
+
+    }
+    private void reverse(int[] a,int start, int end ){
+        while(start<end){
+            int temp=a[start];
+            a[start]=a[end];
+            a[end]=temp;
+            start++;
+            end--;
+        }
+
+    }
+}
