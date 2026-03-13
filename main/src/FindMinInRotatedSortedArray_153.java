eccd8fba97a6162b554aeb8b941518d79367774f
Zhanyl <123janyl456@gmail.com>
2026-03-13 19:02:02 +0100

    leetcode 153

---
+++ main/src/FindMinInRotatedSortedArray_153.java
@@ -0,0 +1,26 @@
+class FindMinInRotatedSortedArray_153 {
+    public int findMin(int[] nums) {
+        int n=nums.length;
+        if( n==0){
+            return -1;
+        }
+        if(nums[0]<nums[n-1]){
+            return nums[0];
+        }
+        int left=0;
+        int right =nums.length-1;
+        while(left<=right){
+            int middle=(left+right)/2;
+            if(middle>0  && nums[middle-1]>nums[middle]){
+                return nums[middle];
+            }
+            if(nums[middle]>nums[right]){
+                left=middle+1;
+            }else{
+                right=middle-1;
+            }
+        }
+        return -1;
+        
+    }
+}
