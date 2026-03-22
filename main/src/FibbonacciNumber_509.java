aa01cd2d914aa8c1c4a18979c3698f5d092c8818
Zhanyl <123janyl456@gmail.com>
2026-03-22 11:07:49 +0100

    improved fibb number

--- main/src/FibbonacciNumber_509.java
+++ main/src/FibbonacciNumber_509.java
@@ -1,17 +1,25 @@
 class FibbonaciNumber_509 {
-    public int fib(int n) {
+    
+        
+    private int[] f;
+    public int fib_(int n) {
         if(n==0){
             return 0;
         }
         else if(n==1){
             return 1;
         }
-        else if(n==2){
-            return 1;
-        }else{
-            return fib(n-1)+fib(n-2);
+        if(f[n]==0){
+            f[n]=fib_(n-1)+fib_(n-2);
         }
+        return f[n];
 
         
+    }
+
+    public int fib(int n){
+        f= new int[n+1];
+        return fib_(n);
+
     }
 }
