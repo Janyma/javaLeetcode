cc1f355d247909835461e52999761a0505af7b5a
Zhanyl <123janyl456@gmail.com>
2026-03-22 20:36:26 +0100

    leetcode 50

---
+++ main/src/MyPow_50.java
@@ -0,0 +1,23 @@
+class MyPow_50 {
+    public double myPow(double x, int n) {
+        long exponent= (long) n;
+
+        if(exponent<0){
+            exponent=-exponent;
+            x=1/x;
+
+
+        }
+        return myPow_( x, exponent);
+    }
+    public double myPow_(double x, long exponent){
+        if(exponent==0){
+            return 1;
+        }
+        if(exponent%2==0){
+            return myPow_(x*x, exponent/2);
+        }else{
+            return myPow_(x*x, exponent/2)*x;
+        }
+    }
+}
