bae275e7203fb33d22272fdf0ca42df8f1c9f060
Zhanyl <123janyl456@gmail.com>
2026-04-19 11:44:38 +0200

    fibbonacci mit memo

--- main/src/FibbonacciNumber_509.java
+++ main/src/FibbonacciNumber_509.java
@@ -1,24 +1,27 @@
+
+import java.util.Arrays;
+
 class FibbonaciNumber_509 {
     
         
-    private int[] f;
+    static int[] mem= new int[100];
     public int fib_(int n) {
         if(n==0){
             return 0;
         }
         else if(n==1){
             return 1;
+        }else if(mem[n]!=-1){
+            return mem[n];
         }
-        if(f[n]==0){
-            f[n]=fib_(n-1)+fib_(n-2);
-        }
-        return f[n];
+            mem[n]= fib_(n-1)+fib_(n-2);
+        return mem[n];
 
         
     }
 
     public int fib(int n){
-        f= new int[n+1];
+        Arrays.fill(mem, -1);
         return fib_(n);
 
     }
