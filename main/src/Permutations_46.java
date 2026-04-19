9ccff4a0364cf42ac83260c526ba8eee42974a3b
Zhanyl <123janyl456@gmail.com>
2026-04-19 12:37:49 +0200

    permutations improvement

--- main/src/Permutations_46.java
+++ main/src/Permutations_46.java
@@ -3,28 +3,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Permutations_46 {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        backtrack(list, new ArrayList(), nums);
-        return list;
-
-        
+    static int n;
+    List<List<Integer>> ans;
+    static void print(int[] ar){
+        System.out.print("[]");
+        for(int i=0; i<n; i++){
+            System.err.print(ar[i]);
+            if(i+1<n){
+                System.out.print(",");
+            }
+        }
+        System.err.println("]");
     }
+    
 
-    public void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums){
-        if(tempList.size()==nums.length){
-            list.add(new ArrayList<>(tempList));
+    static void fun(int[] nums, int[]arr, int ind, List<List<Integer>>ans){
+        if(ind==n){
+            List<Integer> copy=new ArrayList<>();
+            for(int i=0; i<n;i++){
+                copy.add(arr[i]);
+            }
+            ans.add(copy);
             return;
         }
-
-        for(int i=0; i<nums.length; i++){
-            if(tempList.contains(nums[i])){
-                continue;
+        System.out.println("nums=");
+        print(arr);
+        for(int i=0; i<n; i++){
+            boolean has=false;
+            for (int j=0; j<ind; j++){
+                if(nums[i]==arr[j]){
+                    has=true;
+                    break;
+                }
             }
-            tempList.add(nums[i]);
-            backtrack(list, tempList, nums);
-            tempList.remove(tempList.size()-1);
+            if(!has){
+                arr[ind]=nums[i];
+                fun(nums, arr, ind+1, ans);
+            }
+            
         }
+        return;
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        n= nums.length;
+        ans=new ArrayList<>();
+        int[] arr=new int[n];
+        fun(nums, arr, 0, ans);
+        return ans;
 
     }
+
+    
 }
